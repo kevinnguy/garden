e704be9c55bd876515323449bfd604284485063b
Kevin Nguy <kevin@macbaby.local>
2018-04-06 09:17:05 -0700

    Add default value for timestamps in postgres

--- migrations/20180403134650_create_entity.js
+++ migrations/20180403134650_create_entity.js
@@ -1,6 +1,6 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("entity", function(t) {
-    t.timestamps();
+    t.timestamps(true, true);
     t.boolean('deleted').defaultTo(false).notNullable();
     t.comment('Not used as an actual table. Only use this for inheritance.');
   });
