4162c29130556223a747f728ef020844d5af7c70
Kevin Nguy <kevin@macbaby.local>
2018-04-30 15:29:56 -0700

    Implement seed file for Nordstrom Rack urls

---
+++ seeds/3_nordstrom_rack_urls.js
@@ -0,0 +1,43 @@
+const table = 'retailer_category_urls';
+
+const dressesCategories = [
+  {
+    name: 'casual',
+    path: 'shop/Women/Clothing/Dresses/Casual',
+  },
+  {
+    name: 'cocktail & party',
+    path: 'shop/Women/Clothing/Dresses/Cocktail%20&%20Party',
+  },
+  {
+    name: 'formal',
+    path: 'shop/Women/Clothing/Dresses/Formal',
+  },
+];
+
+exports.seed = async function(knex) {
+  await knex(table).del();
+
+  const retailer = await knex('retailers').where({
+    name: 'Nordstrom Rack',
+  }).first();
+
+  const promises = dressesCategories.map(async obj => {
+    const { name, path } = obj;
+
+    const category = await knex('categories').where({
+      name,
+    }).first();
+
+    return {
+      path,
+      hostname: retailer.hostname,
+      scheme: retailer.scheme,
+      retailerId: retailer.id,
+      category_id: category.id,
+    };
+  });
+
+  const json = await Promise.all(promises);
+  await knex(table).insert(json);
+};
