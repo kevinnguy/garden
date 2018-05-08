81a3223b804c550de807d8a3d7fb9ed26bf3726f
Kevin Nguy <kevin@macbaby.local>
2018-05-08 13:32:43 -0700

    Create Profile model

---
+++ models/Profile.js
@@ -0,0 +1,56 @@
+const { Model } = require('objection');
+
+class Profile extends Model {
+  static get tableName() {
+    return 'profiles';
+  }
+
+  static get virtualAttributes() {
+    return ['fullName'];
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      // required: ['title', 'price', 'retailerUrlPath', 'retailerId'],
+
+      properties: {
+        id: { type: 'string', nullable: false, primary: true },
+        email: { type: 'string' },
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+        username: { type: 'string' },
+        phone: { type: 'string' },
+        karma: { type: 'number', nullable: false, minimum: 0 },
+        imageUrl: { type: 'string' },
+      },
+    };
+  };
+
+  static get relationMappings() {
+    return {
+      // retailer: {
+      //   relation: Model.BelongsToOneRelation,
+      //   modelClass: __dirname + '/Retailer',
+      //   join: {
+      //     from: 'products.retailerId',
+      //     to: 'retailers.id',
+      //   },
+      // },
+      // images: {
+      //   relation: Model.HasManyRelation,
+      //   modelClass: __dirname + '/ProductImage',
+      //   join: {
+      //     from: 'products.id',
+      //     to: 'product_images.productId',
+      //   },
+      // },
+    };
+  }
+}
+
+module.exports = Profile;
