d2df50935675005d49deb92d47b312a23a4c994c
Kevin Nguy <kevin@macbaby.local>
2018-05-10 17:24:43 -0700

    Rename checkout_products table to product_checkout_metadata

---
+++ migrations/20180510172309_rename_product_checkout_metadata.js
@@ -0,0 +1,7 @@
+exports.up = async function(knex) {
+  await knex.schema.renameTable('checkout_products', 'product_checkout_metadata');
+};
+
+exports.down = async function(knex) {
+await knex.schema.renameTable('product_checkout_metadata', 'checkout_products');
+};
