9a1c0ba26b6f3f654231863d3e908f8490e1f406
Kevin Nguy <kevin@macbaby.local>
2018-05-14 13:16:45 -0700

    Add purchased_at column in product_checkout_metadata table

---
+++ migrations/20180514131510_add_purchased_at_column_product_checkout_metadata_table.js
@@ -0,0 +1,11 @@
+exports.up = async function(knex) {
+  await knex.schema.table('product_checkout_metadata', t => {
+    t.timestamp('purchased_at');
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.table('product_checkout_metadata', t => (
+    t.dropColumn('purchased_at');
+  ));
+};
