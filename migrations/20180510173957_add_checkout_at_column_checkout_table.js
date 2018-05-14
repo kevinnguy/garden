50181f2ae2cd3ce633365ad52ef38bc397341ca9
Kevin Nguy <kevin@macbaby.local>
2018-05-14 13:16:30 -0700

    Add add_checkout_at column in checkouts table

---
+++ migrations/20180510173957_add_checkout_at_column_checkout_table.js
@@ -0,0 +1,11 @@
+exports.up = async function(knex) {
+  await knex.schema.table('checkouts', t => {
+    t.timestamp('checkout_at');
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.table('checkouts', t => (
+    t.dropColumn('checkout_at');
+  ));
+};
